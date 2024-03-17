e15634777ce57891dd16ff9e5c3218c8c83e8ef2
poey11 <mocpayao15@gmail.com>
2024-03-18 03:47:08 +0800

    added the basic redirection. can proceed to the implementation of features

---
+++ app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.mobdeve.s13.payao.malcolm.fitme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+
+    private  lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnItemSelectedListener{menuItem ->
+            when(menuItem.itemId) {
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+
+                R.id.bottom_calendar -> {
+                    replaceFragment(CalendarFragment())
+                    true
+                }
+
+                R.id.bottom_history -> {
+                    replaceFragment(HistoryFragment())
+                    true
+                }
+
+                R.id.bottom_user -> {
+                    replaceFragment(UserFragment())
+                    true
+                }
+                else-> false
+            }
+        }
+
+        replaceFragment(HomeFragment())
+
+
+    }
+
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frame_container, fragment).commit()
+
+    }
+}
