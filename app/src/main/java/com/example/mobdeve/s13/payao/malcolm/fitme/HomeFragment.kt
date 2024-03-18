bc480bc3816883b69df42a86bf4d9f2790020ba6
poey11 <mocpayao15@gmail.com>
2024-03-18 14:07:06 +0800

    added the basic layout for HomeFragment, will add the recyclerView, workoutTemplate, and adapter

--- app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/HomeFragment.kt
+++ app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 
 class HomeFragment : Fragment() {
@@ -12,9 +14,21 @@ class HomeFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val fab: FloatingActionButton = view.findViewById(R.id.floatingActionButton)
+
+        fab.setOnClickListener{
+            Toast.makeText(requireContext(), "BOBO MO DERRICK", Toast.LENGTH_SHORT).show()
+        }
     }
 
 
