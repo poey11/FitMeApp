1f24a453f625f4a7d2aaecd85c73f6414d44c2d8
justanne <justine_anne_y_rosete@dlsu.edu.ph>
2024-03-25 17:45:52 +0800

    updated the save button in the settings

--- app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/CalendarFragment.kt
+++ app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/CalendarFragment.kt
@@ -1,5 +1,6 @@
 package com.example.mobdeve.s13.payao.malcolm.fitme
 
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,13 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import java.util.*
 import kotlin.collections.ArrayList
+import androidx.recyclerview.widget.LinearLayoutManager
 
-import android.util.Log
+
 
 class CalendarFragment : Fragment(), CalendarAdapter.OnItemListener {
 
@@ -25,11 +27,11 @@ class CalendarFragment : Fragment(), CalendarAdapter.OnItemListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_calendar, container, false)
         initWidgets(view)
         selectedDate = Calendar.getInstance()
         setMonthView()
+
         return view
     }
 
@@ -48,15 +50,19 @@ class CalendarFragment : Fragment(), CalendarAdapter.OnItemListener {
         }
     }
 
+
     private fun setMonthView() {
         monthYearText.text = monthYearFromDate(selectedDate)
         val daysInMonth = daysInMonthArray(selectedDate)
+        val currentMonth = selectedDate.get(Calendar.MONTH)
+        val currentYear = selectedDate.get(Calendar.YEAR)
 
-        val calendarAdapter = CalendarAdapter(daysInMonth, this)
+        val calendarAdapter = CalendarAdapter(daysInMonth, this, currentMonth, currentYear)
         val layoutManager = GridLayoutManager(requireContext(), 7)
         calendarRecyclerView.layoutManager = layoutManager
         calendarRecyclerView.adapter = calendarAdapter
     }
+
 
     private fun daysInMonthArray(date: Calendar): ArrayList<String> {
         val daysInMonthArray = ArrayList<String>()
@@ -79,7 +85,6 @@ class CalendarFragment : Fragment(), CalendarAdapter.OnItemListener {
             daysInMonthArray.add("")
         }
 
-        Log.d("CalendarAdapter", "Days in month: $daysInMonthArray")
         return daysInMonthArray
     }
 
@@ -101,8 +106,49 @@ class CalendarFragment : Fragment(), CalendarAdapter.OnItemListener {
 
     override fun onItemClick(position: Int, dayText: String?) {
         if (!dayText.isNullOrEmpty()) {
-            val message = "Selected Date $dayText ${monthYearFromDate(selectedDate)}"
-            Toast.makeText(requireContext(), message, Toast.LENGTH_LONG).show()
+            val dialog = MyBottomSheetDialogFragment.newInstance(dayText)
+            dialog.show(childFragmentManager, "MyBottomSheetDialogFragment")
+        }
+    }
+
+
+
+    class MyBottomSheetDialogFragment : BottomSheetDialogFragment() {
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+            val view = inflater.inflate(R.layout.calendar_bottomsheet, container, false)
+
+            // Initialize the RecyclerView
+            val recyclerView = view.findViewById<RecyclerView>(R.id.scheduledExercises)
+
+            // Set the layout manager for your RecyclerView
+            val layoutManager = LinearLayoutManager(requireContext())
+            recyclerView.layoutManager = layoutManager
+
+            // Initialize the list of exercises
+            val exercises = ScheduledExerciseDataHelper.initializeExercises()
+
+            // Create the adapter with the exercises list
+            val adapter = ScheduledExerciseAdapter(exercises)
+
+            // Set the adapter to your RecyclerView
+            recyclerView.adapter = adapter
+
+            return view
+        }
+
+
+        companion object {
+            fun newInstance(dayText: String): MyBottomSheetDialogFragment {
+                val fragment = MyBottomSheetDialogFragment()
+                val args = Bundle()
+                args.putString("selectedDay", dayText)
+                fragment.arguments = args
+                return fragment
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/ScheduledExerciseAdapter.kt
@@ -0,0 +1,26 @@
+package com.example.mobdeve.s13.payao.malcolm.fitme
+
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ScheduledExerciseAdapter(private val exercises: ArrayList<ScheduledExercise>) :
+    RecyclerView.Adapter<ScheduledExerciseViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ScheduledExerciseViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val view: View = inflater.inflate(R.layout.item_scheduled_exercise, parent, false)
+        return ScheduledExerciseViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ScheduledExerciseViewHolder, position: Int) {
+        val exercise = exercises[position]
+        holder.bind(exercise)
+    }
+
+    override fun getItemCount(): Int {
+        return exercises.size
+    }
+}
