7928d70fc0f4b7f1393a2fa8b599a2c4020eb260
poey11 <mocpayao15@gmail.com>
2024-03-31 22:10:28 +0800

    revised vieworkout layout

--- app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/models/CExerciseViewHolder.kt
+++ app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/models/CExerciseViewHolder.kt
@@ -1,18 +1,61 @@
 package com.example.mobdeve.s13.payao.malcolm.fitme.models
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.ContextCompat.startActivity
+import androidx.core.content.res.ResourcesCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mobdeve.s13.payao.malcolm.fitme.R
 
 
 
 class CExerciseViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-   private val tvExerciseTitle: TextView = itemView.findViewById(R.id.exerciseTitle)
-
+    private val tvExerciseTitle: TextView = itemView.findViewById(R.id.exerciseTitle)
+    private  val infoBtn : Button = itemView.findViewById(R.id.infoBtn)
+    private  val incBtn : Button = itemView.findViewById(R.id.increaseBtn)
+    private  val decBtn : Button = itemView.findViewById(R.id.decreaseBtn)
     fun bind(exercise: String) {
+        var nos = 2
+        val mainLayout :LinearLayout = itemView.findViewById(R.id.linearLayout2)
         tvExerciseTitle.text = exercise
+        infoBtn.setOnClickListener {
+            val intent = Intent(itemView.context, Instructions::class.java).apply {
+                putExtra("exerciseTitle", exercise)
+            }
+            itemView.context.startActivity(intent)
+        }
+        incBtn.setOnClickListener {
+            val newLinearLayout =  LinearLayout(itemView.context)
+            newLinearLayout.orientation = LinearLayout.HORIZONTAL
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            newLinearLayout.layoutParams = params
 
+            val setTv = TextView(itemView.context)
+            val setTvParams = LinearLayout.LayoutParams(
+                R.dimen.defWidth,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            setTv.text = nos.toString()
+            setTv.textSize = R.dimen.defTextSize.toFloat()
+            setTv.typeface = ResourcesCompat.getFont(itemView.context, R.font.josefin_sans_semibold)
+            setTv.layoutParams = setTvParams
+
+
+            nos++
+            newLinearLayout.addView(setTv)
+            mainLayout.addView(newLinearLayout)
+        }
+        decBtn.setOnClickListener {
+            mainLayout.removeViewAt(mainLayout.childCount-1)
+        }
     }
+
 }
--- app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/models/Instructions.kt
+++ app/src/main/java/com/example/mobdeve/s13/payao/malcolm/fitme/models/Instructions.kt
@@ -15,107 +15,6 @@ import com.example.mobdeve.s13.payao.malcolm.fitme.database.ExerciseDataHelper
 import android.util.Log
 
 
-/*
-class Instructions: AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        setContentView(R.layout.instructions_history)
-
-        // Retrieve the exercise title from the intent extras
-        val exerciseTitle = intent.getStringExtra("exerciseTitle")
-
-        // Find the TextView with id exerciseTV and set its text to the exercise title
-        val exerciseTextView = findViewById<TextView>(R.id.exerciseTV)
-        exerciseTextView.text = exerciseTitle
-
-
-
-        // Find the instructionsBtn button
-        val statsBtn: Button = findViewById(R.id.statsBtn)
-
-        // Set OnClickListener to the instructionsBtn button
-        statsBtn.setOnClickListener {
-            // Launch the InstructionsActivity with exercise title as an extra
-            val intent = Intent(this, Stats::class.java)
-            intent.putExtra("exerciseTitle", exerciseTitle)
-            startActivity(intent)
-        }
-
-
-    }
-}
-*/
-/*
-class Instructions : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.instructions_history)
-
-        // Retrieve the exercise title and instructions from the intent extras
-        val exerciseTitle = intent.getStringExtra("exerciseTitle")
-        val instruction = intent.getStringExtra("instruction")
-
-        // Find the TextViews with id exerciseTV and instructionStepsTv and set their text
-        val exerciseTextView = findViewById<TextView>(R.id.exerciseTV)
-        val instructionStepsTv = findViewById<TextView>(R.id.instructionstepsTv)
-
-        exerciseTextView.text = exerciseTitle
-        instructionStepsTv.text = instruction
-
-        // Find the statsBtn button
-        val statsBtn: Button = findViewById(R.id.statsBtn)
-
-        // Set OnClickListener to the statsBtn button
-        statsBtn.setOnClickListener {
-            // Launch the Stats activity with exercise title as an extra
-            val intent = Intent(this, Stats::class.java)
-            intent.putExtra("exerciseTitle", exerciseTitle)
-            startActivity(intent)
-        }
-    }
-}
-
- */
-
-/*
-class Instructions : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.instructions_history)
-
-        // Retrieve the exercise title and instructions steps from the intent extras
-        val exerciseTitle = intent.getStringExtra("exerciseTitle")
-        val instructionsSteps = intent.getStringExtra("instruction")
-
-        // Find the TextViews to display exercise title and instructions steps
-        val exerciseTextView = findViewById<TextView>(R.id.exerciseTV)
-        val instructionsStepsTextView = findViewById<TextView>(R.id.instructionstepsTv)
-
-        // Set the text of TextViews
-        exerciseTextView.text = exerciseTitle
-        instructionsStepsTextView.text = instructionsSteps.toString()
-
-        // Find the statsBtn button
-        val statsBtn: Button = findViewById(R.id.statsBtn)
-
-        // Set OnClickListener to the statsBtn button
-        statsBtn.setOnClickListener {
-
-            finish()
-        }
-
-        val backBtn: Button = findViewById(R.id.backBtn)
-        backBtn.setOnClickListener{
-            finish()
-        }
-
-    }
-}*/
-
 
 class Instructions : AppCompatActivity() {
 
@@ -127,21 +26,7 @@ class Instructions : AppCompatActivity() {
     // Firebase Firestore instance
     private val db = FirebaseFirestore.getInstance()
 
-    /*override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.instructions_history)
 
-        // Retrieve the exercise title from the intent extras
-        exerciseTitle = intent.getStringExtra("exerciseTitle") ?: ""
-        instructionTextView = findViewById(R.id.instructionstepsTv)
-
-        // Set the exercise title in the TextView
-        val exerciseTextView = findViewById<TextView>(R.id.exerciseTV)
-        exerciseTextView.text = exerciseTitle
-
-        // Fetch and display the instructions
-        fetchInstructions()
-    }*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
